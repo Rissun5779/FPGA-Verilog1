3d6a8f4355713deb7ad19ddaf97faa0b52f08df3
Rissun5779 <rissun57@gmail.com>
2026-02-24 10:55:58 +0800

    update 0224 sim

---
+++ 0224/Adder/Tb_Adder0.sv
@@ -0,0 +1,21 @@
+`timescale 1 ns/1 ns
+module Tb_Adder0;
+  reg A, B, C;
+  wire D, E;
+//Unit Under Test instance and port map
+Adder0 UUT (.x(A), .y(B), .Cin(C), .Cout(D), .Sum(E));
+initial begin  //stimulus generation block
+    A = 1'b0; B = 1'b0; C = 1'b0;
+    #10;  A = 1'b0; B = 1'b0; C = 1'b1;        
+    #10;  A = 1'b0; B = 1'b1; C = 1'b1;        
+    #10;  A = 1'b1; B = 1'b0; C = 1'b1;        
+    #10;  A = 1'b1; B = 1'b1; C = 1'b1;        
+    #10;  A = 1'b0; B = 1'b1; C = 1'b0;
+    #10;  A = 1'b1; B = 1'b0; C = 1'b0;
+    #10;  A = 1'b1; B = 1'b1; C = 1'b0;
+    #10;
+end
+initial #100 $finish;
+initial  //response monitoring block
+  $monitor($realtime, "ns %h %h %h %h", A, B, C, {D, E});
+endmodule
